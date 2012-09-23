4fd84daee238f289f527190262ae12da7305eaf8
rued <rued@e849b3e2-e4dc-4056-93de-471848b6b4fa>
2012-09-23 10:05:49 +0000

    Noe gikk feil, forsøker på nytt ...

--- 3.0/bokmaal_nb-NO/tinymce_nb-NO/tinymce/jscripts/tiny_mce/plugins/autosave/langs/nb.js
+++
@@ -1,4 +0,0 @@
-tinyMCE.addI18n('nb.autosave',{
-restore_content: "Gjenopprett autolagret innhold",
-warning_message: "Om du gjenoppretter lagret innhold vil du miste gjeldende endringer.\n\nEr du sikker på at du vil gjenopprette lagret innhold?"
-});

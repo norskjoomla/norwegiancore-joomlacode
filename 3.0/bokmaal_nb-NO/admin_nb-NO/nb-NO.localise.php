002aba0c754a7ae2f95c021958cb98d5a0a62b10
rued <rued@e849b3e2-e4dc-4056-93de-471848b6b4fa>
2012-11-04 18:43:48 +0000

    Oppdatert og klar for 3.0.0 (Roar/Rune)

--- 3.0/bokmaal_nb-NO/admin_nb-NO/nb-NO.localise.php
+++ 3.0/bokmaal_nb-NO/admin_nb-NO/nb-NO.localise.php
@@ -1,20 +1,22 @@
 <?php
 /**
- * @version		$Id$
- * @copyright	Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
- * @copyright Copyright (C) Translation 2005 - 2012 Joomla! i Norge. All rights reserved.
- * @license		GNU General Public License version 2 or later; see LICENSE.txt
+ * @package    Joomla.Language
+ * @version    $Id$
+ * @copyright  Copyright (C) 2005 - 2012 Open Source Matters, Inc. All rights reserved.
+ * @copyright  Copyright (C) Translation 2005 - 2012 Joomla! i Norge. All rights reserved.
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 defined('_JEXEC') or die;
 
 /**
- * nb-NO Localise class
+ * nb-NO localise class
  *
- * @package		Joomla.Site
- * @since		1.6
+ * @package  Joomla.Language
+ * @since    1.6
  */
-abstract class nb_NOLocalise {
+abstract class nb_NOLocalise
+{
 	/**
 	 * Returns the potential suffixes for a specific number of items
 	 *
@@ -22,15 +24,15 @@ abstract class nb_NOLocalise {
 	 * @return	array  An array of potential suffixes.
 	 * @since	1.6
 	 */
-	public static function getPluralSuffixes($count) {
-		if ($count == 0) {
-			$return =  array('0');
+	public static function getPluralSuffixes($count)
+	{
+		if ($count == 0)
+		{
+			$return = array('0');
 		}
-		elseif($count == 1) {
-			$return =  array('1');
-		}
-		elseif($count > 8 && $count < 12) {
-			$return = array('TEN', 'MORE');
+		elseif($count == 1)
+		{
+			$return = array('1');
 		}
 		else {
 			$return = array('MORE');
@@ -43,13 +45,44 @@ abstract class nb_NOLocalise {
 	 * @return	array  An array of ignored search words.
 	 * @since	1.6
 	 */
-	public static function getIgnoreSearchWords() {
+	public static function getIgnoredSearchWords()
+	{
 		$search_ignore = array();
 		$search_ignore[] = "og";
 		$search_ignore[] = "i";
 		$search_ignore[] = "på";
 		$search_ignore[] = "den";
 		return $search_ignore;
+	}
+	/**
+	 * Returns the lower length limit of search words
+	 *
+	 * @return	integer  The lower length limit of search words.
+	 * @since	1.6
+	 */
+	public static function getLowerLimitSearchWord()
+	{
+		return 3;
+	}
+	/**
+	 * Returns the upper length limit of search words
+	 *
+	 * @return	integer  The upper length limit of search words.
+	 * @since	1.6
+	 */
+	public static function getUpperLimitSearchWord()
+	{
+		return 20;
+	}
+	/**
+	 * Returns the number of chars to display when searching
+	 *
+	 * @return	integer  The number of chars to display when searching.
+	 * @since	1.6
+	 */
+	public static function getSearchDisplayedCharactersNumber()
+	{
+		return 200;
 	}
 	/**
 	 * This method processes a string and replaces all accented UTF-8 characters by unaccented
@@ -117,4 +150,3 @@ abstract class nb_NOLocalise {
 		return $str;
 	}
 }
-
